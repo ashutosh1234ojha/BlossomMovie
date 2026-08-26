c9e5bcd8014962cc7eaaa887a9114f49dc0b06a2
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2026-08-26 19:34:55 +1000

    Refactor: code optimized

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -18,7 +18,7 @@ struct HomeView: View {
                 case .notStarted:
                     EmptyView()
                 case .fetching:
-                    ProgressView()
+                    ProgressView().frame(width: geo.size.width,height: geo.size.height)
                 case .success:
                     LazyVStack {
                         AsyncImage(url: URL(string: heroTestTitle)){image in
--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -29,22 +29,27 @@ class ViewModel{
     func getTitle() async {
         homeStatus = .fetching
         
-        do{
-            async let tMovies = dataFetcher.fetchTitle(for: "movie", by: "trending")
-            async let tTV =  dataFetcher.fetchTitle(for: "tv", by: "trending")
-            async let tRMovies =  dataFetcher.fetchTitle(for: "movie", by: "top_rated")
-            async let tRTV =  dataFetcher.fetchTitle(for: "tv", by: "top_rated")
+        if trendingMovies.isEmpty{
             
-            trendingMovies =  try await tMovies
-            trendingTV =  try await tTV
-            topRatedMovies =  try await tRMovies
-            topRatedTV =  try await tRTV
-            
+            do{
+                async let tMovies = dataFetcher.fetchTitle(for: "movie", by: "trending")
+                async let tTV =  dataFetcher.fetchTitle(for: "tv", by: "trending")
+                async let tRMovies =  dataFetcher.fetchTitle(for: "movie", by: "top_rated")
+                async let tRTV =  dataFetcher.fetchTitle(for: "tv", by: "top_rated")
+                
+                trendingMovies =  try await tMovies
+                trendingTV =  try await tTV
+                topRatedMovies =  try await tRMovies
+                topRatedTV =  try await tRTV
+                
+                homeStatus = .success
+                
+            }catch{
+                print(error)
+                homeStatus = .failed(underlyingError: error)
+            }
+        } else {
             homeStatus = .success
-            
-        }catch{
-            print(error)
-            homeStatus = .failed(underlyingError: error)
         }
     }
 }
