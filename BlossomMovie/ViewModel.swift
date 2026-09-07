7b34dd6ec81463b725cb8089bc8a8b3fcea31423
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2026-09-07 23:01:16 +1000

    Feat: api added for upcoming movies

--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -19,6 +19,7 @@ class ViewModel{
     
     private(set) var homeStatus: FetchStatus = .notStarted
     private(set) var videoIdStatus: FetchStatus = .notStarted
+    private(set) var upcomingStatus: FetchStatus = .notStarted
     
     private let dataFetcher = DataFetcher()
     
@@ -26,6 +27,7 @@ class ViewModel{
     var trendingTV:[Title] = []
     var topRatedMovies:[Title] = []
     var topRatedTV:[Title] = []
+    var upcomingMovies:[Title] = []
     var heroTitle : Title = Title.previewTitles[0]
     var videoId = ""
     
@@ -71,4 +73,17 @@ class ViewModel{
             videoIdStatus = .failed(underlyingError: error)
         }
     }
+    
+    func getUpcomingMovies() async {
+        upcomingStatus = .fetching
+        
+        do{
+            upcomingMovies =  try await dataFetcher.fetchTitle(for: "movie", by: "upcoming")
+            upcomingStatus = .success
+        }catch{
+            print(error)
+            upcomingStatus = .failed(underlyingError: error)
+        }
+    }
+   
 }
